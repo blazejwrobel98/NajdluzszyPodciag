8a962131ad702786c7af2bca6747affacb8668ed
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2020-06-06 18:21:36 +0200

    fail

--- NajdluzszyPodciag/Program.cs
+++ NajdluzszyPodciag/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 class Program
 {
     public static void Main()
@@ -16,70 +16,90 @@ class Program
         }
         int[] array_input = int_input.ToArray();
         int n = array_input.Length;
-        LIS(array_input, n);
-    }
-    static void LIS(int[] arr, int n)
-    {
-        int[] lis_arr = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            lis_arr[i] = 0;
-        }
-        int[] prevIndices = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            prevIndices[i] = -1;
-        }
-        int len = 1;
-        for (int i = 1; i < n; i++)
-        {
-            if (arr[i] < arr[lis_arr[0]])
-            {
-                lis_arr[0] = i;
-            }
-            else if (arr[i] > arr[lis_arr[len - 1]])
-            {
-                prevIndices[i] = lis_arr[len - 1];
-                lis_arr[len++] = i;
-            }
-            else
-            {
-                int pos = GetCeilIndex(arr, lis_arr, -1, len - 1, arr[i]);
-
-                prevIndices[i] = lis_arr[pos - 1];
-                lis_arr[pos] = i;
-            }
-        }
-        Console.WriteLine("Podany ciąg to:");
-        foreach (int el in arr)
+        int[] result = LIS(array_input, n);
+        foreach(int el in result)
         {
             Console.Write(el + " ");
         }
-        Console.Write("\n");
-        Console.WriteLine("Najdłuższy podciąg rosnący to:");
-        string output = "";
-        for (int i = lis_arr[len - 1]; i >= 0; i = prevIndices[i])
-        {
-            output += (arr[i] + " ");
-        }
-        string[] output_arr = output.Split(' ');
-        for (int x = (output_arr.Length) - 1; x >= 0; x--)
-        {
-            Console.Write(output_arr[x] + " ");
-        }
-        Console.Write("\n");
     }
-    static int GetCeilIndex(int[] arr, int[] T, int l, int r, int key)
+    static int[] LIS(int[] arr, int n)
     {
-        while (r - l > 1)
+        int[] thelowest = GetLowest(arr, n); //Pobieram punkty gdzie funkcja ma startować
+        List<int[]> foundsubstrings = new List<int[]>();
+        foreach (int el in thelowest)
         {
-            int m = l + (r - l) / 2;
-            if (arr[T[m]] >= key)
-                r = m;
-            else
-                l = m;
+            foundsubstrings.Add(FindSub(arr, el, n));
         }
-        return r;
+        List<int> subs_length = new List<int>();
+        int x = foundsubstrings.Count;
+        for (int y = 0; y < x; y++)
+        {
+            subs_length.Add(foundsubstrings[y].Length);
+        }
+        int[] subs_len = subs_length.ToArray();
+        int v = subs_len.Max();
+        int pos = 0;
+        for(int b = 0; b < subs_len.Length; b++)
+        {
+            if (subs_len[b] == v)
+            {
+                pos = b;
+                break;
+            }
+        }
+        return foundsubstrings[pos];
+
+    }
+    static int[] FindSub(int[] arr, int pos, int n)
+    {
+        List<int> temp = new List<int>();
+        for (int i = pos; i < n; i++)
+        {
+            if (i + 1 < n)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    temp.Add(arr[i]);
+                }
+            }
+            else if(i+1==n)
+            {
+                if (arr[n-2] < arr[n-1])
+                {
+                    temp.Add(arr[n-1]);
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        int[] output = temp.ToArray();
+        return output;
+    }
+    static int[] GetLowest(int[] arr, int n)
+    {
+        var low_position = new List<int>();
+        int min = arr[0];
+        //Szukam wartości minimalnej
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        //Szukam powtórzeń wartości minimalnej np {1,2,3,1}
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == min)
+            {
+                low_position.Add(i);
+            }
+        }
+        //Zwracam pozycje najniższych wartości
+        int[] low_arr = low_position.ToArray();
+        return low_arr;
     }
     static int GetInt()
     {
